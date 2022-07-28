6f157cfc05d473f087e4d844b90f45638f292004
tomzcn <tomz@163.com>
2022-07-28 22:34:17 +0800

    1

---
+++ tomzheng.js
@@ -0,0 +1,10 @@
+function tomzheng(){
+    return [
+  {"title":"tomzheng/index","created":"20220727202301847",
+  "modified":"20220727202301847",
+  "tags":"tomzheng",
+  "type":"text/vnd.tiddlywiki",
+  "text":"tomzheng's homepage\r\n"
+  }
+  ];
+  }
